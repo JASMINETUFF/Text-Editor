f82289164d077f0324d24308f55dd893a175fec2
Jasmine Santa Maria <89921853+JASMINETUFF@users.noreply.github.com>
2022-02-12 16:27:03 -0800

    Update database.js
    Updated the method on line 27 that gets content from the IndexedDB database using the idb module

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,7 +15,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
+  console.error('PUT into the database');
   const jateDB = await openDB ('jate', 1);
   const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
@@ -24,6 +24,17 @@ export const putDb = async (content) => {
   console.log(' - Data saved to the database 😎', result.value);
 };
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('- Data retrieved from the Database 😎', reset.value)
+    : console.log('- Data was not found in the Database');
+return result?.value;
+};
 
 initdb();
