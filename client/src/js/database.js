469cec7f5630f53b59529094e471959a747c4478
Jasmine Santa Maria <89921853+JASMINETUFF@users.noreply.github.com>
2022-02-12 22:40:08 -0800

    Updates to files
    add and confirgure workp=vox plugins for service worker and manifest file
    add css loaders and babel to webpack
    
    ran some test
    cleaned up syntax errors

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,4 @@
 import { openDB } from 'idb';
-import { reset } from 'nodemon';
 
 const initdb = async () =>
   openDB('jate', 1, {
@@ -13,28 +12,29 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Method that takes some content and adds it to the IndexedDB database using the idb module
 export const putDb = async (content) => {
-  console.error('PUT into the database');
-  const jateDB = await openDB ('jate', 1);
-  const tx = jateDB.transaction('jate', 'readwrite');
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log(' - Data saved to the database 😎', result.value);
+  console.log('🚀 - data saved to the database', result.value);
 };
-// TODO: Add logic for a method that gets all the content from the database
+
+// Method that gets content from the IndexedDB database using the idb module
 export const getDb = async () => {
-  console.error('GET from the database');
+  console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
   result
-    ? console.log('- Data retrieved from the Database 😎', reset.value)
-    : console.log('- Data was not found in the Database');
-return result?.value;
+    ? console.log(' ⛳️ - data retrieved from the database', result.value)
+    : console.log('🙅🏻 - data not found in the database');
+  return result?.value;
 };
 
 initdb();
