2837111ab65fee654a66ab976142d1184728ebc8
Jasmine Santa Maria <89921853+JASMINETUFF@users.noreply.github.com>
2022-02-20 07:30:49 -0800

    updates to text editor

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -31,7 +31,6 @@ export default class {
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
       putDb(localStorage.getItem('content'));
